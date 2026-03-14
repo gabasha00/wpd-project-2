377ddc07aa5ee74f4aee8a7bdbd3019e6911cf84
legenddoros <lackey.scott@gmail.com>
2026-03-13 21:30:51 -0400

    added earthquake API

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // server.js
 const express = require("express");
+const fetch = require("node-fetch");
 const nunjucks = require("nunjucks");
+
 const app = express();
 
 app.use(express.static("public")); // serve static files
@@ -20,6 +22,7 @@ app.get("/index", (req, res) => {
   res.render("index.njk");
 });
 
+// waves api endpoint
 app.get("/api/waves", async (req, res) => {
   try {
     const response = await fetch(
@@ -30,6 +33,22 @@ app.get("/api/waves", async (req, res) => {
   } catch (error) {
     console.error("NOAA fetch error:", error);
     res.status(500).json({ error: "Wave data failed" });
+  }
+});
+
+// earthquakes api endpoint
+app.get("/api/earthquakes", async (req, res) => {
+  try {
+    const response = await fetch(
+      "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson",
+    );
+
+    const data = await response.json();
+
+    res.json(data);
+  } catch (error) {
+    console.error("USGS fetch error:", error);
+    res.status(500).send(error.toString());
   }
 });
 
